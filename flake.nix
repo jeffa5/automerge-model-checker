f4e5326706de4e6cdc6c5415df05fc74cf7b8fd5
Andrew Jeffery <dev@jeffas.io>
2022-05-18 13:31:18 +0100

    Add flake config

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Automerge Model Checking";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            overlays = [ rust-overlay.overlay ];
+            inherit system;
+          };
+          lib = pkgs.lib;
+          rust = pkgs.rust-bin.stable.latest.default;
+          cargoNix = pkgs.callPackage ./Cargo.nix {
+            inherit pkgs;
+            release = true;
+          };
+          debugCargoNix = pkgs.callPackage ./Cargo.nix {
+            inherit pkgs;
+            release = false;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                (rust.override {
+                  extensions = [ "rust-src" ];
+                })
+                cargo-edit
+                cargo-watch
+                cargo-criterion
+                cargo-fuzz
+                cargo-flamegraph
+                cargo-deny
+                crate2nix
+
+                rnix-lsp
+                nixpkgs-fmt
+              ];
+          };
+        });
+}
