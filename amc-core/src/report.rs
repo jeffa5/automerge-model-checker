9e672e970336067a4f4d6a456e4614e047dce3f3
Andrew Jeffery <dev@jeffas.io>
2023-01-18 17:43:48 +0000

    Add nicer number formatting in reporter

--- amc-core/src/report.rs
+++ amc-core/src/report.rs
@@ -1,3 +1,5 @@
+use num_format::SystemLocale;
+use num_format::ToFormattedString;
 use stateright::Model;
 
 /// A reporter with more information about the rate of new states being processed.
@@ -13,19 +15,20 @@ where
 {
     fn report_checking(&mut self, data: stateright::report::ReportData) {
         let new_total = data.total_states - self.last_total;
-        let total_rate = (data.total_states as f64 / data.duration.as_secs_f64()).round();
+        let total_rate = (data.total_states as f64 / data.duration.as_secs_f64()).round() as u64;
         let new_unique = data.unique_states - self.last_unique;
-        let unique_rate = (data.unique_states as f64 / data.duration.as_secs_f64()).round();
+        let unique_rate = (data.unique_states as f64 / data.duration.as_secs_f64()).round() as u64;
         let status = if data.done { "Done    " } else { "Checking" };
+        let locale = SystemLocale::default().unwrap();
         println!(
-            "{} states={: >8} (+{: <8} {: >8.0}/s), unique={: >8} (+{: <8} {: >8}/s), duration={:?}",
+            "{} states={: >8} (+{: <8} {: >8}/s), unique={: >8} (+{: <8} {: >8}/s), duration={:?}",
             status,
-            data.total_states,
-            new_total,
-            total_rate,
-            data.unique_states,
-            new_unique,
-            unique_rate,
+            data.total_states.to_formatted_string(&locale),
+            new_total.to_formatted_string(&locale),
+            total_rate.to_formatted_string(&locale),
+            data.unique_states.to_formatted_string(&locale),
+            new_unique.to_formatted_string(&locale),
+            unique_rate.to_formatted_string(&locale),
             data.duration
         );
 
