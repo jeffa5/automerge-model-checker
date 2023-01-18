66bdc5bc16f3741faa3635dc01bc711db6412f0b
Andrew Jeffery <dev@jeffas.io>
2023-01-18 15:06:56 +0000

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,50 +3,55 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            overlays = [ rust-overlay.overlays.default ];
-            inherit system;
-          };
-          lib = pkgs.lib;
-          rust = pkgs.rust-bin.stable.latest.default;
-          cargoNix = pkgs.callPackage ./Cargo.nix {
-            inherit pkgs;
-            release = true;
-          };
-          debugCargoNix = pkgs.callPackage ./Cargo.nix {
-            inherit pkgs;
-            release = false;
-          };
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs;
-              [
-                (rust.override {
-                  extensions = [ "rust-src" ];
-                })
-                cargo-edit
-                cargo-watch
-                cargo-criterion
-                cargo-fuzz
-                cargo-flamegraph
-                cargo-deny
-                crate2nix
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [rust-overlay.overlays.default];
+        inherit system;
+      };
+      lib = pkgs.lib;
+      rust = pkgs.rust-bin.stable.latest.default;
+      cargoNix = pkgs.callPackage ./Cargo.nix {
+        inherit pkgs;
+        release = true;
+      };
+      debugCargoNix = pkgs.callPackage ./Cargo.nix {
+        inherit pkgs;
+        release = false;
+      };
+    in {
+      formatter = pkgs.alejandra;
 
-                rnix-lsp
-                nixpkgs-fmt
-              ];
-          };
-        });
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (rust.override {
+            extensions = ["rust-src"];
+          })
+          cargo-edit
+          cargo-watch
+          cargo-criterion
+          cargo-fuzz
+          cargo-flamegraph
+          cargo-deny
+          crate2nix
+
+          rnix-lsp
+          nixpkgs-fmt
+        ];
+      };
+    });
 }
