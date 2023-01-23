f42445cf9e4b909e9d66e8f50003feaaa169f335
Andrew Jeffery <dev@jeffas.io>
2023-01-23 14:02:41 +0000

    Move model creation to modelopts

--- crates/amc/src/cli.rs
+++ crates/amc/src/cli.rs
@@ -1,15 +1,10 @@
 use clap::Parser;
 use stateright::{
-    actor::{model_peers, ActorModel, Id, Network},
     Checker, Model,
 };
 
 use crate::{
-    application::server::Server,
-    client::Client,
-    global::GlobalActor,
     model::{ModelBuilder, ModelOpts},
-    properties,
     report::Reporter,
 };
 
@@ -37,61 +32,10 @@ pub struct RunArgs {
 
     /// Model opts
     #[clap(flatten)]
-    model_opts: ModelOpts,
+    pub model_opts: ModelOpts,
 }
 
 impl RunArgs {
-    fn actor_model<M: ModelBuilder>(
-        &self,
-        model_builder: &M,
-    ) -> ActorModel<GlobalActor<M::App, M::Driver>, M::Config, M::History> {
-        let config = model_builder.config(&self.model_opts);
-        println!("Built config: {:?}", config);
-        let history = model_builder.history();
-        println!("Built history: {:?}", history);
-        let mut model = ActorModel::new(config, history);
-
-        // add servers
-        for i in 0..self.model_opts.servers {
-            model = model.actor(GlobalActor::Server(Server {
-                peers: model_peers(i, self.model_opts.servers),
-                sync_method: self.model_opts.sync_method,
-                app: model_builder.application(i),
-            }))
-        }
-
-        // add drivers
-        for i in 0..self.model_opts.servers {
-            for driver in model_builder.drivers(i) {
-                model = model.actor(GlobalActor::Client(Client {
-                    server: Id::from(i),
-                    driver,
-                    _app: std::marker::PhantomData,
-                }));
-            }
-        }
-
-        if self.model_opts.in_sync_check {
-            model = properties::with_in_sync_check(model);
-        }
-        if self.model_opts.save_load_check {
-            model = properties::with_save_load_check(model);
-        }
-        if self.model_opts.error_free_check {
-            model = properties::with_error_free_check(model);
-        }
-
-        for property in model_builder.properties() {
-            model = model.property(property.expectation, property.name, property.condition);
-        }
-        let record_request = model_builder.record_input();
-        let record_response = model_builder.record_output();
-        model
-            .record_msg_in(record_request)
-            .record_msg_out(record_response)
-            .init_network(Network::new_ordered(vec![]))
-    }
-
     /// Run an application.
     pub fn run<M: ModelBuilder>(self, model_builder: M)
     where
@@ -102,7 +46,8 @@ impl RunArgs {
         println!("{:?}", self);
         println!("{:?}", model_builder);
         let model = self
-            .actor_model(&model_builder)
+            .model_opts
+            .to_model(&model_builder)
             .checker()
             .threads(num_cpus::get());
 
--- crates/amc/src/model.rs
+++ crates/amc/src/model.rs
@@ -1,11 +1,11 @@
 use crate::{
-    application::{server::SyncMethod, Application},
+    application::{server::{SyncMethod, Server}, Application},
     driver::Drive,
-    global::{GlobalActor, GlobalMsg},
+    global::{GlobalActor, GlobalMsg}, client::Client, properties,
 };
 use clap::Args;
 use stateright::{
-    actor::{ActorModel, Envelope},
+    actor::{ActorModel, Envelope, model_peers, Id, Network},
     Property,
 };
 use std::fmt::Debug;
@@ -87,4 +87,60 @@ pub struct ModelOpts {
     /// Enable checking documents don't panic.
     #[clap(long, global = true)]
     pub error_free_check: bool,
+}
+
+impl ModelOpts {
+    /// Create a model to use for checking.
+    ///
+    /// Intended for use in tests.
+    pub fn to_model<M: ModelBuilder>(
+        &self,
+        model_builder: &M,
+    ) -> ActorModel<GlobalActor<M::App, M::Driver>, M::Config, M::History> {
+        let config = model_builder.config(&self);
+        println!("Built config: {:?}", config);
+        let history = model_builder.history();
+        println!("Built history: {:?}", history);
+        let mut model = ActorModel::new(config, history);
+
+        // add servers
+        for i in 0..self.servers {
+            model = model.actor(GlobalActor::Server(Server {
+                peers: model_peers(i, self.servers),
+                sync_method: self.sync_method,
+                app: model_builder.application(i),
+            }))
+        }
+
+        // add drivers
+        for i in 0..self.servers {
+            for driver in model_builder.drivers(i) {
+                model = model.actor(GlobalActor::Client(Client {
+                    server: Id::from(i),
+                    driver,
+                    _app: std::marker::PhantomData,
+                }));
+            }
+        }
+
+        if self.in_sync_check {
+            model = properties::with_in_sync_check(model);
+        }
+        if self.save_load_check {
+            model = properties::with_save_load_check(model);
+        }
+        if self.error_free_check {
+            model = properties::with_error_free_check(model);
+        }
+
+        for property in model_builder.properties() {
+            model = model.property(property.expectation, property.name, property.condition);
+        }
+        let record_request = model_builder.record_input();
+        let record_response = model_builder.record_output();
+        model
+            .record_msg_in(record_request)
+            .record_msg_out(record_response)
+            .init_network(Network::new_ordered(vec![]))
+    }
 }
