d4404df495126f36f337143c38768be46afc4988
Andrew Jeffery <dev@jeffas.io>
2023-01-30 18:12:37 +0000

    Add basic bench script

---
+++ bench.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+cargo build --release
+
+results_dir=results
+rm -rf $results_dir
+mkdir -p $results_dir
+
+function info() {
+  echo "=========== $@ ============="
+}
+
+function run() {
+  bin=$1
+  shift
+  filename=$@
+  filename="${filename// /_}"
+  out="$results_dir/$filename"
+  cmd="cargo run -q --release --bin $bin -- $@ | tee $out"
+  info $cmd
+  eval $cmd
+}
+
+info counter
+run amc-counter check-iterative
+run amc-counter check-iterative --counter-type
+run amc-counter check-iterative --initial-change
+run amc-counter check-iterative --initial-change --counter-type
+
+info moves
+run amc-moves check-iterative
+
+info todos
+run amc-todo check-iterative
+run amc-todo check-iterative --random-ids
+run amc-todo check-iterative --initial-change
+run amc-todo check-iterative --initial-change --random-ids
+
+info automerge
+run amc-automerge check-iterative
