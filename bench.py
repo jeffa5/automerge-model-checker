cf5fe374454b52fd96a3a4a36bd4ab4a651ca2a3
Andrew Jeffery <dev@jeffas.io>
2023-02-01 22:53:31 +0000

    Add servers

--- bench.py
+++ bench.py
@@ -22,17 +22,18 @@ def make_results_dir():
 class Config:
     bin_name: str
     sync_method: str
+    servers: int
     extra_flags: List[str]
 
     def dir(self) -> str:
-        d = f"{self.bin_name}_{self.sync_method}"
+        d = f"{self.bin_name}_{self.sync_method}_{self.servers}"
         if self.extra_flags:
             d += "_"
             d += "_".join(self.extra_flags)
         return d
 
     def to_args(self) -> str:
-        a = f"--sync-method={self.sync_method}"
+        a = f"--sync-method={self.sync_method} --servers={self.servers}"
         if self.extra_flags:
             a += " "
             a += " ".join([f"--{n}" for n in self.extra_flags])
@@ -57,120 +58,143 @@ def main():
     make_results_dir()
 
     sync_methods = ["changes", "messages", "save-load"]
-    for sync_method in sync_methods:
-        # amc-counter
-        run(Config(bin_name="amc-counter", sync_method=sync_method, extra_flags=[]))
-        run(
-            Config(
-                bin_name="amc-counter",
-                sync_method=sync_method,
-                extra_flags=["counter-type"],
+    for servers in [2, 3]:
+        for sync_method in sync_methods:
+            # amc-counter
+            run(
+                Config(
+                    bin_name="amc-counter",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=[],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-counter",
-                sync_method=sync_method,
-                extra_flags=["initial-change"],
+            run(
+                Config(
+                    bin_name="amc-counter",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["counter-type"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-counter",
-                sync_method=sync_method,
-                extra_flags=["counter-type", "initial-change"],
+            run(
+                Config(
+                    bin_name="amc-counter",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["initial-change"],
+                )
             )
-        )
+            run(
+                Config(
+                    bin_name="amc-counter",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["counter-type", "initial-change"],
+                )
+            )
 
-        # amc-moves
-        run(
-            Config(
-                bin_name="amc-moves",
-                sync_method=sync_method,
-                extra_flags=[],
+            # amc-moves
+            run(
+                Config(
+                    bin_name="amc-moves",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=[],
+                )
             )
-        )
 
-        # amc-todo
-        run(
-            Config(
-                bin_name="amc-todo",
-                sync_method=sync_method,
-                extra_flags=[],
+            # amc-todo
+            run(
+                Config(
+                    bin_name="amc-todo",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=[],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-todo",
-                sync_method=sync_method,
-                extra_flags=["random-ids"],
+            run(
+                Config(
+                    bin_name="amc-todo",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["random-ids"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-todo",
-                sync_method=sync_method,
-                extra_flags=["initial-change"],
+            run(
+                Config(
+                    bin_name="amc-todo",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["initial-change"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-todo",
-                sync_method=sync_method,
-                extra_flags=["initial-change", "random-ids"],
+            run(
+                Config(
+                    bin_name="amc-todo",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["initial-change", "random-ids"],
+                )
             )
-        )
 
-        # amc-automerge
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["bytes"],
+            # amc-automerge
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["bytes"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["string"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["string"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["int"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["int"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["uint"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["uint"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["timestamp"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["timestamp"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["boolean"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["boolean"],
+                )
             )
-        )
-        run(
-            Config(
-                bin_name="amc-automerge",
-                sync_method=sync_method,
-                extra_flags=["null"],
+            run(
+                Config(
+                    bin_name="amc-automerge",
+                    sync_method=sync_method,
+                    servers=servers,
+                    extra_flags=["null"],
+                )
             )
-        )
 
 
 if __name__ == "__main__":
